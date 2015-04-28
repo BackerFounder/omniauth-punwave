be365c4837864a8d8ee51f1087782aba0d9fa6b7
Stan Low <a22301613@yahoo.com.tw>
2015-04-28 08:41:26 +0800

    update raw_info method

--- lib/omniauth/strategies/punwave.rb
+++ lib/omniauth/strategies/punwave.rb
@@ -1,41 +1,42 @@
 require "omniauth/strategies/oauth2"
- 
+
 module OmniAuth
   module Strategies
     class Punwave < OmniAuth::Strategies::OAuth2
       option :name, :punwave
- 
+
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-        site:  "http://stage.member.punwave.com"
+        site:  "http://stage.member.punwave.com",
+        response_type: :code
       }
- 
+
       # Disable state-based CSRF protection
       option :provider_ignores_state, true
- 
+
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
       uid { raw_info["id"] }
- 
+
       info do
         {
           nickname: raw_info["nickname"],
           email: raw_info["email"]
         }
       end
- 
+
       extra do
         {
           "raw_info" => raw_info.reject { |k, v| ["nickname", "email"].include?(k) }
         }
       end
- 
+
       def raw_info
-        @raw_info ||= access_token.get("oauth/me.json").parsed
+        @raw_info ||= access_token.get("api/v1/profile").parsed
       end
     end
   end
